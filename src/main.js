0fae2af8f8c8a9f9c4ff4acbf74ec7fc86a50ec2
Rininmmmment <rinr10693otes@gmail.com>
2024-06-20 01:55:19 +0000

    add: first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import './app.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import { initializeApp } from "firebase/app";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VUE_APP_API_KEY,
+  authDomain: import.meta.env.VUE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VUE_APP_PROJECTID,
+  storageBucket: import.meta.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VUE_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+createApp(App).mount('#app')
